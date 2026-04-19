02c2ff03cacb00f1cc8a5a01fae6084f8a930909
Adam Kačmár <113941012+adamkacmar@users.noreply.github.com>
2026-04-19 22:27:30 +0200

    feat: added lightweight configs for LiDAR-only (32 beam and 4 beam)

--- tools/create_data.py
+++ tools/create_data.py
@@ -246,7 +246,6 @@ if __name__ == '__main__':
             out_dir=args.out_dir,
             with_plane=args.with_plane)
     elif args.dataset == 'nuscenes' and args.version != 'v1.0-mini':
-        '''
         train_version = f'{args.version}-trainval'
         nuscenes_data_prep(
             root_path=args.root_path,
@@ -264,6 +263,7 @@ if __name__ == '__main__':
             dataset_name='NuScenesDataset',
             out_dir=args.out_dir,
             max_sweeps=args.max_sweeps)
+        '''
     elif args.dataset == 'nuscenes' and args.version == 'v1.0-mini':
         train_version = f'{args.version}'
         nuscenes_data_prep(
