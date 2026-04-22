8502df2051bca9d9f765b2b3e07b48f913104f1d
Adam Kačmár <113941012+adamkacmar@users.noreply.github.com>
2026-04-22 20:43:11 +0200

    fix?: switched from fudet to futr3d imports, fixed config issues with TensorBoard, switch back the number of channels for one config due to initial baseline run

--- plugin/futr3d/configs/lidar_only/lidar_0075v_300q.py
+++ plugin/futr3d/configs/lidar_only/lidar_0075v_300q.py
@@ -1,7 +1,7 @@
 _base_ = [
-    '../../../configs/_base_/datasets/nus-3d.py',
-    '../../../configs/_base_/schedules/cyclic_20e.py', 
-    '../../../configs/_base_/default_runtime.py'
+    '../../../../configs/_base_/datasets/nus-3d.py',
+    '../../../../configs/_base_/schedules/cyclic_20e.py', 
+    '../../../../configs/_base_/default_runtime.py'
 ]
 
 plugin = 'plugin/futr3d'
@@ -15,7 +15,7 @@ point_cloud_range = [-54, -54, -5.0, 54, 54, 3.0]
 
 center_head = dict(
     type='CenterHead',
-    in_channels=sum([128, 128]),
+    in_channels=sum([256, 256]),
     tasks=[
         dict(num_class=1, class_names=['car']),
         dict(num_class=2, class_names=['truck', 'construction_vehicle']),
@@ -55,17 +55,17 @@ model = dict(
         type='SparseEncoder',
         in_channels=5,
         sparse_shape=[41, 1440, 1440],
-        output_channels=64,
+        output_channels=128,
         order=('conv', 'norm', 'act'),
-        encoder_channels=((16, 16, 32), (32, 32, 64), (64, 64, 96), (96,
-                                                                      96)),
+        encoder_channels=((16, 16, 32), (32, 32, 64), (64, 64, 128), (128,
+                                                                      128)),
         encoder_paddings=((0, 0, 1), (0, 0, 1), (0, 0, [0, 1, 1]), (0, 0)),
         block_type='basicblock'),
     pts_backbone=dict(
         type='SECOND',
-        in_channels=128,
-        out_channels=[64, 128],
-        layer_nums=[3, 3],
+        in_channels=256,
+        out_channels=[128, 256],
+        layer_nums=[5, 5],
         layer_strides=[1, 2],
         norm_cfg=dict(type='BN', eps=1e-3, momentum=0.01),
         conv_cfg=dict(type='Conv2d', bias=False)),
@@ -73,8 +73,8 @@ model = dict(
         type='FPN',
         norm_cfg=dict(type='BN2d', eps=1e-3, momentum=0.01),
         act_cfg=dict(type='ReLU', inplace=False),
-        in_channels=[64, 128],
-        out_channels=128,
+        in_channels=[128, 256],
+        out_channels=256,
         start_level=0,
         add_extra_convs=True,
         num_outs=4,
@@ -89,7 +89,7 @@ model = dict(
         mix_selection=False,
         num_query=300,
         num_classes=10,
-        in_channels=128,
+        in_channels=256,
         pc_range=point_cloud_range,
         sync_cls_avg_factor=True,
         with_box_refine=True,
@@ -325,3 +325,10 @@ find_unused_parameters=True
 custom_hooks = [dict(type='FadeOjectSampleHook', num_last_epochs=5)]
 
 checkpoint_config = dict(interval=1, max_keep_ckpts=1)
+
+# Keep only text logging to avoid TensorBoard import issues in this environment.
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook')
+    ])
