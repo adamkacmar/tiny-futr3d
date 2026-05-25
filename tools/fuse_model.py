ff1751054eb07c9e55eacedd0c2c0f8ad53195fa
Adam Kačmár <113941012+adamkacmar@users.noreply.github.com>
2026-05-25 18:21:58 +0200

    feat: rewritten fuse_model.py

--- tools/fuse_model.py
+++ tools/fuse_model.py
@@ -1,15 +1,29 @@
+import argparse
+
 import torch
 
-img_ckpt = torch.load('work_dirs/vovnet_trainval/epoch_24.pth')
-state_dict1 = img_ckpt['state_dict']
 
-pts_ckpt = torch.load('work_dirs/co_dab_lidar_0075v_900q_trainval/epoch_20.pth')
-state_dict2 = pts_ckpt['state_dict']
-# pts_head in camera checkpoint will be overwrite by lidar checkpoint
-state_dict1.update(state_dict2)
+def parse_args():
+	parser = argparse.ArgumentParser(description='Fuse image and lidar checkpoints')
+	parser.add_argument('--img', required=True, help='Path to the image checkpoint')
+	parser.add_argument('--lidar', required=True, help='Path to the lidar checkpoint')
+	parser.add_argument('--out', required=True, help='Output checkpoint path')
+	return parser.parse_args()
 
-merged_state_dict = state_dict1
 
-save_checkpoint = {'state_dict':merged_state_dict }
+def main():
+	args = parse_args()
 
-torch.save(save_checkpoint, 'checkpoint/lidar_vov_trainval.pth')
+	img_ckpt = torch.load(args.img, map_location='cpu')
+	pts_ckpt = torch.load(args.lidar, map_location='cpu')
+
+	state_dict = img_ckpt['state_dict'].copy()
+	# Lidar checkpoint keys should overwrite matching image checkpoint keys.
+	state_dict.update(pts_ckpt['state_dict'])
+
+	save_checkpoint = {'state_dict': state_dict}
+	torch.save(save_checkpoint, args.out)
+
+
+if __name__ == '__main__':
+	main()
