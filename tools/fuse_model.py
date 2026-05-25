79c32a1142f7ee57bef088c49fd938731809551c
Adam Kačmár <113941012+adamkacmar@users.noreply.github.com>
2026-05-25 18:56:15 +0200

    fix?: changed fuse_model.py declaration

--- tools/fuse_model.py
+++ tools/fuse_model.py
@@ -15,13 +15,16 @@ def main():
 	args = parse_args()
 
 	img_ckpt = torch.load(args.img, map_location='cpu')
+	state_dict1 = img_ckpt['state_dict']
+
 	pts_ckpt = torch.load(args.lidar, map_location='cpu')
+	state_dict2 = pts_ckpt['state_dict']
+	# pts_head in camera checkpoint will be overwrite by lidar checkpoint
+	state_dict1.update(state_dict2)
 
-	state_dict = img_ckpt['state_dict'].copy()
-	# Lidar checkpoint keys should overwrite matching image checkpoint keys.
-	state_dict.update(pts_ckpt['state_dict'])
+	merged_state_dict = state_dict1
 
-	save_checkpoint = {'state_dict': state_dict}
+	save_checkpoint = {'state_dict':merged_state_dict }
 	torch.save(save_checkpoint, args.out)
 
 
